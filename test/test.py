ed77a1819862e538d25313c360d734dd13b70ec7
LigniFabri <conexionfabricio@hotmail.com>
2020-07-08 20:09:33 -0500

    setup y precondiciones

--- test/test.py
+++ test/test.py
@@ -1,12 +1,26 @@
 import unittest
 
+
+
 class Mis_test(unittest.TestCase):
 
+    term_uno=0
+    term_dos=0
+    correr=True
+
+    def setUp(self):
+        self.term_uno = 2
+        self.term_dos = 2
+        if self.term_uno * self.term_dos == 2000:
+            self.correr = False
+
     def test_suma(self):
-        self.assertEqual(2+2,4)
-    
+        self.assertEqual(self.term_uno+self.term_dos,4)
+        self.assertTrue(self.correr)
+
     def test_resta(self):
-        self.assertTrue(1-1==0)
+        self.assertTrue(self.term_uno-self.term_dos==0)
+        self.assertTrue(self.correr)
 
 if __name__=='__main__':
     unittest.main()
