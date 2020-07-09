8dbb59042ab39efddc2a4e696faa0f465603ccdd
LigniFabri <conexionfabricio@hotmail.com>
2020-07-08 20:35:10 -0500

    assertEqual y assertNotEqual

--- test/test.py
+++ test/test.py
@@ -22,6 +22,16 @@ class Mis_test(unittest.TestCase):
         self.assertTrue(self.term_uno-self.term_dos==0)
         self.assertTrue(self.correr)
 
+    def test_verificar_iguales(self):
+        a=5
+        b=4+1
+        self.assertEqual(a,b)
+
+    def test_verificar_no_iguales(self):
+        a=19
+        b=5*3
+        self.assertNotEqual(a,b,"a tiene que ser diferente de b")
+
     def tearDown(self):
         print('Borrando datos.......')
 
